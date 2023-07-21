39092e214cabc4ec9b04a33c5c209f688f4f9f64
Mark Muchai <muchaimark6@gmail.com>
2023-07-21 13:10:38 +0300

    initial commit

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,14 +3,44 @@ import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  const [filterBy, setFilterBy] = useState("All");
+    const foodsToDisplay = foods.filter((food) => {
+      if (filterBy === "All") {
+        return true;
+      } else {
+        return food.cuisine === filterBy;
+      }
+    });
+
+    
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+    const newFoodArray = [...foods, newFood];
+    setFoods(newFoodArray);
   }
 
-  const foodList = foods.map((food) => (
-    <li key={food.id}>
+
+  
+
+
+
+  function handleClick(id) {
+    const newFoodArray = foods.map((food) => {
+      if (food.id === id) {
+        return {
+          ...food,
+          heatLevel: food.heatLevel + 1,
+        };
+      } else {
+        return food;
+      }
+    });
+    setFoods(newFoodArray);
+  }
+  
+  const foodList = foodsToDisplay.map((food) => (
+    <li key={food.id} onClick={()=> handleClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
